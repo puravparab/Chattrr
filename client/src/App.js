d8f1b2d9a67f7a8d9325880eecd6aea81176c012
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-01-16 22:29:31 +0400

    changes to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route, Link} from 'react-router-dom'
 // import PrivateRoute from './utilities/PrivateRoute.js'
 import Register from "./pages/Register"
 import LogIn from "./pages/LogIn"
 
+// TODO: Fix url refresh
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<h1 className="title">Chattrr</h1>
-				<div>
-					<Link to="/register">Register</Link> |{" "}
-					<Link to="/login">Log In</Link>
-				</div>
-				<Routes>
-					<Route path='/register' element={<Register/>} />
-					<Route path='/login' element={<LogIn />} />
-				</Routes>
+		<div className="App">
+			<h1 className="title">Chattrr</h1>
+			<div>
+				<Link to="/register">Register</Link> |{" "}
+				<Link to="/login">Log In</Link>
 			</div>
-		</Router>
+			<Routes>
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<LogIn />} />
+			</Routes>
+		</div>
 	);
 }
 
