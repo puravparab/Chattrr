725ee9b0425563e7e2a60d9cd46f5286b50db44e
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-02-13 16:36:59 +0400

    created a login form template

--- client/src/components/forms/LogInForm.js
+++ client/src/components/forms/LogInForm.js
@@ -4,6 +4,23 @@ import '../../styles/components/forms/loginform.css';
 const LogInForm = () => {
     return (
         <div className="login-form">
+            <div className="title">
+                <h1>Sign in to your account</h1> 
+            </div>
+           
+            <form className="form">
+                <div className="form-entry">
+                    <label className="label">Username</label>
+                    <input />
+                </div>
+
+                <div className="form-entry">
+                    <label className="label">password</label>
+                    <input />
+                </div>
+
+                <button onClick="" className="btn-submit" type="submit">Log In</button>
+            </form>
         </div>
     );
 }
