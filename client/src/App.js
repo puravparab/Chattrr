e90eba9219699e5bb74429c66e11efc1e5f8223b
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-02-19 20:34:43 +0400

    added homepage and conditional rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route, Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Routes, Route, Link, Navigate} from 'react-router-dom'
 // import PrivateRoute from './utilities/PrivateRoute.js'
 import HomePage from "./pages/HomePage"
 import Register from "./pages/Register"
@@ -8,6 +8,14 @@ import Error404Page from "./pages/Error404Page"
 
 // TODO: Fix url refresh
 function App() {
+	// const [tokens, setTokens] = useState({
+	// 	access_token: '',
+	// 	refresh_token: ''
+	// }
+	
+	// TODO: fix Auth token usage
+	const [isAuth, setIsAuth] = useState(false)
+
 	return (
 		<div className="App">
 			<h1 className="title">Chattrr</h1>
@@ -16,9 +24,12 @@ function App() {
 				<Link to="/login">Log In</Link>
 			</div>
 			<Routes>
-				<Route path='/register' element={<Register />} />
-				<Route path='/login' element={<LogIn />} />
-				<Route path='/' element={<LogIn />} />
+				<Route exact path='/register' element={<Register />} />
+				<Route exact path='/login' element={<LogIn />} />
+				{ isAuth ? 
+					<Route path='/' element={<HomePage />} /> : 
+					<Route path='/' element={<Navigate replact to='/login' />} />
+				}
 				<Route path='*' element={<Error404Page />} />
 			</Routes>
 		</div>
