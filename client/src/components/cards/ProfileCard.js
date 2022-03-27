c9c8cca6bb0662008fd6064f47c9f3e34588ca7b
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-03-28 01:57:46 +0400

    profile card dynamically updates

--- client/src/components/cards/ProfileCard.js
+++ client/src/components/cards/ProfileCard.js
@@ -1,22 +1,38 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { dateDifference } from "../../utilities/time.js"
 import defaultPFP from '../../assets/images/default-pfp.png';
 
 const ProfileCard = (props) => {
 	let navigate = useNavigate();
-	
+	const [accessToken, setAccessToken] = useState(props.accessToken)
+
 	return (
 		<div className="profile-card">
 			<div className="profile-card-left">
-				<img src={defaultPFP} width="32" height="32" alt={`${props.username}'s profile picture`} onClick={()=>{
-					navigate(`/user/${props.username}`);
+				<img src={defaultPFP} width="32" height="32" alt={`${props.user.username}'s profile picture`} onClick={()=>{
+					navigate(`/user/${props.user.username}`);
 				}}/>
 			</div>
 			<div className="profile-card-right">
 				<div className="profile-header">
+					<div className="header-detail">
+						<div className="username" onClick={()=>{
+							navigate(`/user/${props.user.username}`);
+						}}>
+							<p>@{props.user.username}</p>
+						</div>
+						<div className="display-name" onClick={()=>{
+							navigate(`/user/${props.user.username}`);
+						}}>
+							<p>{props.user.display_name}</p>
+						</div>
+					</div>
 				</div>
 				<div className="profile-body">
 				</div>
 				<div className="profile-footer">
+					<p>Joined {dateDifference(props.user.created_at)}</p>
 				</div>
 			</div>
 		</div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from "react-router-dom"
 import { getToken, isAuthenticated } from  "../actions/authActions.js"
 import ProfileCard from "../components/cards/ProfileCard"
 import ProfileFeed from "../components/ProfileFeed"
 import '../styles/pages/profile.css';
 import backBtn from '../assets/icons/white_arrow.svg';
+
+const ROOT_URL = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port
 
 const Profile = () =>{
 	let params = useParams()
@@ -23,6 +25,38 @@ const Profile = () =>{
 		}
 	})
 
+	const [profile, setProfile] = useState('')
+
+	useEffect(() => {
+		//Attempt to retreive data
+		try {
+			getUserDetail()
+		}
+		// TODO: Test This
+		catch (e) {
+			console.log(e)
+		}
+	}, [])
+
+	const getUserDetail = async () => {
+		const res = await fetch(ROOT_URL + '/accounts/' + params.username, {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json',
+				'Authorization': "Bearer " + accessToken
+			}
+		})
+		const data = await res.json()
+		if(res.ok){
+			const profileCard = <ProfileCard 
+									user={data.user}
+									accessToken={accessToken} />
+			setProfile(profileCard)
+		}else{
+			navigate('../error')
+		}
+	}
+
 	return (
 		<div className="home">
 			<div className="header">
@@ -38,7 +72,7 @@ const Profile = () =>{
 			</div>
 			<div className="profile-container">
 				<div className="profile-container-center">
-					<ProfileCard />
+					{profile}
 					<ProfileFeed accessToken={accessToken} username={params.username} renderComment={true} />
 				</div>
 			</div>
