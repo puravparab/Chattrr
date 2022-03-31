0c3f3cc62adfdec88c26084603260317c4308837
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-03-31 05:31:53 +0400

    added profile link card to home

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import { getToken, isAuthenticated } from  "../actions/authActions.js"
 import Feed from "../components/Feed"
 import PostBlurtForm from "../components/forms/PostBlurtForm"
+import defaultPFP from '../assets/images/default-pfp.png';
+import moreIcon from '../assets/icons/more_icon_white.svg';
 import '../styles/pages/homepage.css';
 
 const HomePage = () => {
@@ -23,11 +25,28 @@ const HomePage = () => {
 
 	return (
 		<div className="home">
-			<div className="title">
-				<h1 onClick={()=> {
-					navigate("/")
-				}}>Chattrr</h1>
+			<div className="home-header">
+				<div className="header-left">
+				</div>
+				<div className="title">
+					<h1 onClick={()=> {
+						navigate("/")
+					}}>Chattrr</h1>
+				</div>
+				<div className="header-right">
+					<div className="link-card">
+						<img src={defaultPFP} width="35" height="35" alt={`s profile picture`} onClick={()=>{
+							navigate(`../user/`);
+						}}/>
+						<div className="user-detail">
+							<p className="display-name">Display Name</p>
+							<p className="username">Username</p>
+						</div>
+						<img className="more" src={moreIcon} width="25" height="25" alt="more icon"/>
+					</div>
+				</div>
 			</div>
+			
 			<div className="home-container">
 				<div className="home-container-center">
 					<PostBlurtForm accessToken={accessToken} />
